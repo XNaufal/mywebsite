4be9da00c016dcd9b93102c818edf54d5ad80ec9
XNaufal <147814762+XNaufal@users.noreply.github.com>
2024-04-16 15:56:01 +0800

    Add files via upload

---
+++ accounts/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.home, name='index'),
+    path('products/', views.product_list, name='product_list'),
+    path('products/<int:product_id>/', views.product_detail, name='product_detail'),
+    path('orders/', views.order_list, name='order_list'),
+    path('orders/<int:order_id>/', views.order_detail, name='order_detail'),
+    path('order/create/', views.order_create, name='order_create'),
+    path('page/<slug:slug>/', views.page_detail, name='page_detail'),
+]
